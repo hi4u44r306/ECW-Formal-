c55bafc25289faca681ff60f116474b6ab06c74d
Victor <hi4u44r306@gmail.com>
2022-12-25 16:15:36 +0800

    .

--- src/components/Slider/Slider.js
+++ src/components/Slider/Slider.js
@@ -3,50 +3,37 @@ import "./Slider.scss";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { sliderImages } from '../../utils/images';
-// import Carousel from 'react-bootstrap/Carousel';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Slider = () => {
+  const interval = 1500;
   return (
     <div className="hero-slider">
       <div className='hero-slider-item'>
-        <img src={sliderImages[0]} alt="" />
-        {/* <Carousel variant="dark">
-          <Carousel.Item>
+        <Carousel variant={null} controls={false} fade="true">
+          <Carousel.Item interval={interval}>
             <img
               className="d-block w-100"
               src={sliderImages[0]}
               alt="First slide"
             />
-            <Carousel.Caption>
-              <h5>First slide label</h5>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item>
+          <Carousel.Item interval={interval}>
             <img
               className="d-block w-100"
               src={sliderImages[1]}
               alt="Second slide"
             />
-            <Carousel.Caption>
-              <h5>Second slide label</h5>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item>
+          <Carousel.Item interval={interval}>
             <img
               className="d-block w-100"
               src={sliderImages[2]}
               alt="Third slide"
             />
-            <Carousel.Caption>
-              <h5>Third slide label</h5>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
           </Carousel.Item>
-        </Carousel> */}
+        </Carousel>
       </div>
     </div>
   )
